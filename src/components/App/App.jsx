a0ee05d489689959148702d81b12aacb539b0303
Vlad Bokii <bokiyvladik10@gmail.com>
2025-03-23 22:16:54 +0200

    hw

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
 const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage"));
 const MovieDetailsPage = lazy(() =>
-  import("../../page/MovieDetailsPages/MovieDetailsPage")
+  import("../../pages/MovieDetailsPages/MovieDetailsPage")
 );
 const MovieCast = lazy(() => import("../MovieCast/MovieCast"));
 const MovieReviews = lazy(() => import("../MovieReviews/MovieReviews"));
